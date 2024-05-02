8507434f0de33e4afec29545592e2eee0a4d3111
Santiago Pineda <tiago01cracy@gmail.com>
2024-05-02 15:44:52 -0500

    chore: Actualizar estilos y ubicación de elementos en el mapa mental

--- src/assets/components/Map.jsx
+++ src/assets/components/Map.jsx
@@ -75,6 +75,66 @@ export function Map() {
         image='img/img7.png'  
         description='Herramientas de videoconferencia'
       />
+      <Item 
+        id='item8'
+        image='img/img8.png'  
+        description='Herramientas colaborativas'
+      />
+      <Item 
+        id='item9'
+        image='img/img9.png'  
+        description='Plataformas de contenido educativo'
+      />
+      <Item 
+        id='item10'
+        image='img/img10.png'  
+        description='Herramientas de evaluación y retroalimentación'
+      />
+      <Item 
+        id='item11'
+        image='img/img11.png'  
+        description='Equilibrio entre lo virtual y lo presencial'
+      />
+      <Item 
+        id='item12'
+        image='img/img12.png'  
+        description='Participación y compromiso de los estudiantes'
+      />
+      <Item 
+        id='item13'
+        image='img/img13.png'  
+        description='Adaptación de los métodos de enseñanza'
+      />
+      <Item 
+        id='item14'
+        image='img/img14.png'  
+        description='Soporte técnico y capacitación'
+      />
+      <Item 
+        id='item15'
+        image='img/img15.png'  
+        description='Acceso equitativo a la tecnología'
+      />
+      <Item 
+        id='item16'
+        image='img/img16.png'  
+        description='reducción de costos'
+      />
+      <Item 
+        id='item17'
+        image='img/img17.png'  
+        description='accecibilidad'
+      />
+      <Item 
+        id='item18'
+        image='img/img18.png'  
+        description='Flexibilidad'
+      />
+      <Item 
+        id='item19'
+        image='img/img19.png'  
+        description=' Los ambientes de aprendizaje  constructivistas proveen múltiples  representaciones de la realidad.'
+      />
 
     </div>
   )
